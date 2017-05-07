5ea38b348b19d558a91248bcf9c024f83f4ee43f
Bon Crowder <SilverDash@Bons-MacBook-Pro.local>
2017-05-06 19:18:57 -0500

    have the two player game boards separated; now just need to get the logic running right and no errors; oh, and a chat

--- assets/javascript/dueling-rps.js
+++ assets/javascript/dueling-rps.js
@@ -17,30 +17,54 @@ var configThisAlready = {
 firebase.initializeApp(configThisAlready);
 
 var theDatabase = firebase.database();
-var dbRef = theDatabase.ref();
-var playerOneRef = dbRef.child("players").child("1").child("choice");
-var playerTwoRef = dbRef.child("players").child("2").child("choice");
+var dbRef = theDatabase.ref()
+var playersRef = theDatabase.ref("players");
+
+
+var playerOneRef = playersRef.child("1");
+var playerTwoRef = playersRef.child("2");
+
+
+var playerOneChoiceRef = playerOneRef.child("choice");
+var playerOneWinsRef = playerOneRef.child("wins");
+var playerOneLossesRef = playerOneRef.child("losses");
+
+
+
+var playerTwoChoiceRef = playerTwoRef.child("choice");
+var playerTwoWinsRef = playerTwoRef.child("wins");
+var playerTwoLossesRef = playerTwoRef.child("losses");
+
+
 
 var choiceList = ["paper","scissors","rock"];
 var playerOneChoice = 17;
-var playerTwoChoice = 17;
 var playerOneName;
+
+var playerTwoChoice = 17;
 var playerTwoName;
+
 var thisUsersNameIsX = "";
 var thisIsPlayerNumberX = 0;
 
+// bmc: User inputs name and clicks Play Button
+// bmc: This assigns the player a player number
 $("#play").on("click", function(e){
     e.preventDefault();
+
+    // bmc: variable for the user
     thisUsersNameIsX = $("#userNameInput").val();
 
+    // bmc: looking to see if there are players around (i.e. logged in and playing) so user can play (or be rejected)
     dbRef.once('value').then(function(snapshot){
-        // bmc: looking to see if there are players around (i.e. logged in and playing)
         areThereAnyPlayersInGame = snapshot.val();
 
         if (!areThereAnyPlayersInGame){
             // bmc: create player 1
-            console.log("there is no player 1");
+            console.log("There are no players, so you get to be player 1.");
             addPlayer(1);
+            thisIsPlayerNumberX = 1;
+            showGamePlay(1);
         }
         else {
             playerOne = snapshot.val().players["1"];
@@ -48,64 +72,83 @@ $("#play").on("click", function(e){
 
             if (!playerOne) {
                 // bmc: create player 1
-                console.log("there is no player 1");
+                console.log("Player 2 is in the house. You can be player 1.");
                 addPlayer(1);
+                $("#you-are-player").html("Player 2 is in the house. You can be player 1.");
+                thisIsPlayerNumberX = 1;
+                showGamePlay(1);
             }
             else if (!playerTwo) {
                 // bmc: create player 2
-                console.log("there is no player 2");
+                console.log("Player 1 is in the house. You can be player 2.");
                 addPlayer(2);
+                $("#you-are-player").html("Player 1 is in the house. You can be player 2.");
+                thisIsPlayerNumberX = 2;
+                showGamePlay(2);
             }
             else {
                 // bmc: display "You can't play now"
-                console.log("you're the third wheel");
+                console.log("Two people are already playing. Please stand by for your turn.");
                 $("#you-are-player").html("Two people are already playing. Please stand by.");
             }
         }
 
     });
 
-    $("#theGame").css("display", "inline");
+    $("#theGame").css("display", "block");
     $(".name-input").css("display", "none");
     $("#greeting").text("Hi there, " + thisUsersNameIsX + "!");
+    $(".just-the-name").text(thisUsersNameIsX);
 });
 
-playerOneRef.on("value", function (snapshot) {
+// bmc: Player X finalizes choice with click
+$("#done-with-choice1").on("click", function(e){
+    e.preventDefault();
+
+    // bmc: set variable "choice" to be the choice user checked
+    var choice = $("input[name='choices']:checked").val();
+
+    // bmc: append the right user with his choice (r, p or s (2, 0 or 1))
+    playersRef.child(1).update({
+        choice: choice
+    });
+    if (playerOneChoice != 17 && playerTwoChoice != 17) {
+        pickTheWinner();
+    }
+});
+
+$("#done-with-choice2").on("click", function(e){
+    e.preventDefault();
+
+    // bmc: set variable "choice" to be the choice user checked
+    var choice = $("input[name='choices']:checked").val();
+
+    // bmc: append the right user with his choice (r, p or s (2, 0 or 1))
+    playersRef.child(2).update({
+        choice: choice
+    });
+    if (playerOneChoice != 17 && playerTwoChoice != 17) {
+        pickTheWinner();
+    }
+});
+
+
+// bmc: when player 1 finalizes choice, set playerOneChoice to the choice
+playerOneChoiceRef.on("value", function (snapshot) {
     if (snapshot.val() != 17 && snapshot.val() != null){
         playerOneChoice = snapshot.val();
         console.log("player one picked " + snapshot.val());
     }
 });
 
-playerTwoRef.on("value", function (snapshot) {
+// bmc: when player 2 finalizes choice, set playerTwoChoice to the choice
+playerTwoChoiceRef.on("value", function (snapshot) {
     if (snapshot.val() != 17 && snapshot.val() != null){
         playerTwoChoice = snapshot.val();
         console.log("player two picked " + snapshot.val());
     }
 });
 
-$("#done-with-choice").on("click", function(e){
-    e.preventDefault();
-
-    var choice = $("input[name='choices']:checked").val();
-    // bmc: append the user thisIsPlayerNumberX with the value
-    theDatabase.ref("players").child(thisIsPlayerNumberX).update({
-        choice: choice
-    });
-
-    dbRef.on('value', function(snapshot){
-        playerOneChoice = snapshot.val().players["1"].choice;
-        playerTwoChoice = snapshot.val().players["2"].choice;
-
-        playerOneName = snapshot.val().players["1"].userName;
-        playerTwoName = snapshot.val().players["2"].userName;
-
-    });
-
-    if (playerOneChoice != 17 && playerTwoChoice != 17) {
-        pickTheWinner();
-    }
-});
 
 // bmc: this gives us the quit button functionality; it works the same as if the user closed the browser
 $("#quit").on("click", function(e) {
@@ -129,7 +172,7 @@ function addPlayer(playerNumberX) {
 
 // bmc: This deletes the user that is registered
 function deleteUser() {
-    theDatabase.ref("players").child(thisIsPlayerNumberX).remove();
+    playersRef.child(thisIsPlayerNumberX).remove();
 }
 
 function pickTheWinner() {
@@ -139,12 +182,14 @@ function pickTheWinner() {
         console.log("You win!");
         // bmc: player one wins
         $("results").html("Player 1 picked " + choiceList[playerOneChoice] + ". <br>Player 2 picked " + choiceList[playerTwoChoice] + ". <br>" + playerOneName + " is the winner!");
+        winner(1);
     }
 
     else if (winDif === -1 || winDif === 2) {
         console.log("You lose!");
         // bmc: player two wins
         $("results").html("Player 1 picked " + choiceList[playerOneChoice] + ". <br>Player 2 picked " + choiceList[playerTwoChoice] + ". <br>" + playerTwoName + " is the winner!");
+        winner(2);
     }
 
     else {
@@ -154,7 +199,34 @@ function pickTheWinner() {
     }
 }
 
-function tally(winner, loser) {
+
+// bmc: tally(number of player that won, number of player that lost)
+function winner(player) {
     // bmc: update winner in firebase with +1
     // bmc: update loser in firebase with +1
+    if (player = 1){
+        playerOneWinsRef.once("value").then (function (snapshot) {
+            playerOneWins = snapshot.val().wins + 1;
+        });
+
+        playerTwoLossesRef.once("value").then (function(snapshot){
+            playerTwoLosses = snapshot.val().losses + 1;
+        });
+    }
+    else {
+        playerTwoWinsRef.once("value").then (function (snapshot) {
+            playerTwoWins = snapshot.val().wins + 1;
+        });
+
+        playerOneLossesRef.once("value").then (function(snapshot){
+            playerOneLosses = snapshot.val().losses + 1;
+        });
+       }
+
+
+}
+
+function showGamePlay(player) {
+    idToChange = "#player" + player;
+    $(idToChange).css("display", "block");
 }
